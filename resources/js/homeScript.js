d7e179e13fefea75e1cdd10934ecefa176a6c2d4
John Tran <jtran129605@gmail.com>
2020-02-07 11:41:40 -0600

    Organized files and threw "signup" into a legacy folder.

---
+++ resources/js/homeScript.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*var modal = document.getElementById('id01');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}*/
+
+
