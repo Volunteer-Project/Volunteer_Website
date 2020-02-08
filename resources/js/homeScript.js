439320821779168b97d20dfd2cadfe878ef4aeb6
John Tran <jtran129605@gmail.com>
2020-02-07 22:24:43 -0600

    added password toggle & password requirements

--- resources/js/homeScript.js
+++ resources/js/homeScript.js
@@ -14,4 +14,21 @@ window.onclick = function(event) {
   }
 }*/
 
+function passwordToggle() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function confirmToggle() {
+  var x = document.getElementById("confirm");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
