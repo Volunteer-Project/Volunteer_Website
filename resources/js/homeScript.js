107d4999b0eadbb326247425f2f646b1fecd0bec
John Tran <jtran129605@gmail.com>
2020-02-27 17:52:02 -0600

    Cleaned up the code

--- resources/js/homeScript.js
+++ resources/js/homeScript.js
@@ -1,19 +1,3 @@
-/*function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-var modal = document.getElementById('id01');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}*/
-
 function passwordToggle() {
   var x = document.getElementById("password");
   if (x.type === "password") {
